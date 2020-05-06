82dd5235fc063c87a7aa6e491d4a0a129434ab19
CesarRojasVasquez <64751808+CesarRojasVasquez@users.noreply.github.com>
2020-05-06 18:14:55 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
  	<!-- boostrap -->
 
 	<link rel="stylesheet" href="assets/animate.css">
-	<link rel="stylesheet" href="assets/style.css">
+	
   	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="wsp.css">
 
